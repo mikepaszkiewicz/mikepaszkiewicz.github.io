8d861cec4768d72d504d3bb5b6be3f5edbbb9a95
mikepaszkiewicz <mike@tryhabitat.com>
2018-10-16 16:14:23 -0400

    bomb

--- scripts/_vti_cnf/AC_RunActiveContent.js
+++
@@ -1,8 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|18 Dec 2006 16:22:52 -0000
-vti_extenderversion:SR|5.0.2.2634
-vti_usagebymonth:UX|9 23 33 19 25 42 67 61 45 51 79 39 24
-vti_usagebyweek:UX|2 5 3 8 5
-vti_usagebyday:UX|0 2 0 1 0 2
-vti_usagelastupdated:TX|15 Feb 2010 09:02:42 -0000
-vti_usagetotalhits:IX|517
